8d47a30655ab333ab638e0170ad3cb3fdab4a4b3
Borcilă Bogdan <91390590+OctoMber26@users.noreply.github.com>
2022-02-27 10:51:17 -0500

    Update Contact.js

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -17,7 +17,7 @@ const Contact=()=>{
             <section id="contact">
         <div className="contact">
             <img className="contact_logo" src={logo} />
-            <h3 className="contact_info">You cand contact me by clicking the icons bellow</h3>
+            <h3 className="contact_info">You can contact me by clicking the icons bellow</h3>
             <a  className="contact_link"  href="https://www.linkedin.com/in/antonia-popescu-0a9678216/" target="_blank"><img className="lk" src={lk} /></a>
             <a  className="contact_link"  href="https://www.instagram.com/lenahills_author/" target="_blank"><img className="instagram" src={instagram} /> </a>
             <a  className="contact_link"  href="https://youtube.com/channel/UCPhTXH14L_MOQBOP1HzYIhw" target="_blank"><img className="youtube" src={youtube} /> </a>
